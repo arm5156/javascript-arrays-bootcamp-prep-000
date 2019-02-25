67795b4c2958f664646f36e8f14e0b2f5707975a
Adam R Miller <arm5156@gmail.com>
2019-02-25 01:08:52 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+function addElementToBeginningOfArray(array, element){
+  return [element,...array]
+  
+  
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
