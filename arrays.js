768913a3da1301277984abfde0690daff829dfcb
Adam R Miller <arm5156@gmail.com>
2019-02-25 01:29:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,14 +23,14 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
    return array
 }
 function removeElementFromBeginningOfArray(array){
-  array.slice(1)
-  return array
+  return array.slice(1)
+  
 }
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
   return array
 }
 function removeElementFromEndOfArray(array){
-  array.slice (0,array.length -1)
-  return array
+  return array.slice (0,array.length -1)
+  
 }
